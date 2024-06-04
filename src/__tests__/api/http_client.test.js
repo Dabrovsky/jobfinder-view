a2bd4c536f12b5e93218a6b9ffe5d44b3d04ffe1
Dabrovski <wojciech.dabrovski@gmail.com>
2024-06-04 19:20:24 +0200

    feat: add base api client

---
+++ src/__tests__/api/http_client.test.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { vi, describe, it, expect, beforeEach } from "vitest";
+import { httpClient } from "../../api/http_client";
+
+vi.mock("axios");
+
+describe("httpClient", () => {
+  let instance;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  beforeEach(() => {
+    axios.create.mockReturnValue({ defaults: { headers } });
+
+    instance = httpClient();
+  });
+
+  it("should create an axios instance", () => {
+    expect(instance).toHaveProperty("defaults");
+  });
+
+  it("should pass headers correctly to axios instance", () => {
+    expect(instance.defaults.headers).toMatchObject(headers);
+  });
+});
